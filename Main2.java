431c8b03604317c888df9e59b39f16296639a307
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-06-30 11:19:17 +0600

    Main2.

---
+++ Main2.java
@@ -0,0 +1,31 @@
+// Вывести все простые числа от 1 до 1000
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+        List<Integer> primes = new ArrayList<>();
+
+        for (int i = 2; i <= input; i++) {
+            boolean isPrimeNumber = true;
+            
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+            if (isPrimeNumber) {
+                primes.add(i);
+            }
+        }
+        System.out.println("Простые числа: " + primes);
+    }
+}
+

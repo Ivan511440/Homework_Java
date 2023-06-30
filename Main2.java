3b7fbab72c9f1d25c681c3f8331790ce722dcc14
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-06-30 11:23:37 +0600

    Main2.

--- Main2.java
+++ Main2.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите положительное число: ");
         int input = scanner.nextInt();
         scanner.close();
         List<Integer> primes = new ArrayList<>();

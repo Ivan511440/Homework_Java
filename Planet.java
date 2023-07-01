87395e77140404861f36fe7926703f2eb1460811
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-07-01 12:53:51 +0600

    Planet.

---
+++ Planet.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Planet {
+    public static void main(String[] args) {
+        ArrayList<String> planets = new ArrayList<>();
+
+        planets.add("Меркурий");
+        planets.add("Венера");
+        planets.add("Земля");
+        planets.add("Марс");
+        planets.add("Юпитер");
+        planets.add("Сатурн");
+        planets.add("Уран");
+        planets.add("Нептун");
+        planets.add("Плутон");
+
+        planets.add("Венера");
+        planets.add("Земля");
+
+        for (String planet : planets) {
+            int count = 0;
+
+            for (String p : planets) {
+                if (planet.equals(p)) {
+                    count ++;
+                }
+            }
+            System.out.printf("%s- %d%n", planet, count);
+        }
+    }
+}
